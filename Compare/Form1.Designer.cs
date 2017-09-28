bb197b2728ddacd4e9b7835b10a82d6969cb4aff
User <User>
2017-09-28 10:58:04 +0400

    compare

---
+++ Compare/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Compare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(158, 77);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(97, 143);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 227);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ Compare/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Compare
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            nameLableandButtons();
+        }
+
+        private void nameLableandButtons() {
+            Random r = new Random();
+            this.button1.Text = r.Next(0, 100).ToString();
+            this.button2.Text = r.Next(0, 100).ToString();
+        }
+
+        private void compareFunction(Button b1 , Button b2) {
+
+            if (Convert.ToInt64(b1.Text) > Convert.ToInt64(b2.Text))
+            {
+                MessageBox.Show("bu button Boyukdur");
+            }
+            else if (Convert.ToInt64(b1.Text) == Convert.ToInt64(b2.Text))
+            {
+                MessageBox.Show("bu buttonlar beraberdir");
+            }
+            else
+            {
+                MessageBox.Show("bu button Kicikdir");
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.compareFunction(this.button1, this.button2);
+
+        }
+       
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.compareFunction(this.button2, this.button1);
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            nameLableandButtons();
+        }
+    }
+}
